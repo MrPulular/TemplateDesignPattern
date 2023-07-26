d1ecf9a89ce5c2e110e14823a6ca966c62fb18b2
kaanpulular@gmail.com <kaanpulular@gmail.com>
2023-07-26 18:23:37 +0300

    Add project files.

---
+++ UserCards/UserCardTemplate.cs
@@ -0,0 +1,38 @@
+﻿using BaseProject.Models;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set;}
+
+        public void SetUser(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        //Karşı inşaa edecek olan metodumuz
+
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+            {
+                var sb = new StringBuilder();
+                sb.Append("<div class ='card'>");
+                sb.Append(SetPicture());
+                sb.Append($@"<div class='card-body'>
+                             <h5>{AppUser.UserName}</h5>
+                             <p>{AppUser.Description}<p>");
+                sb.Append(SetFooter());
+                sb.Append("</div>");
+                sb.Append("</div>");
+
+                return sb.ToString();
+            }
+        }
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+
+    }
+}
